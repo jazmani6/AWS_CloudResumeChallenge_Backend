2517056334fa3e91b5ee9ed4167578a673915b8e
jazmani6 <35899766+jazmani6@users.noreply.github.com>
2023-03-07 10:42:24 -0500

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -9,13 +9,13 @@ terraform {
     bucket = "jmresume.com"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-2"
-    shared_credentials_file = "~/.aws/credentials"
+    
     
   }
 }
 
  provider "aws" {
   region = "us-east-2"
-  shared_credentials_file = "~/.aws/credentials"
+ 
   
 }
